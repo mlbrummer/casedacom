d9bea5fe9f29e08835f799719795c0d9360d458c
mlbrummer <maaikebrummer96@gmail.com>
2022-06-05 12:45:07 +0200

    created overall base html and html for app weather. Added form to fill in longitude and latitude coordinates. Added simple home function in views to get coordinates, call meteoserver api with those coordinates, return weather data and show results.

--- weather/views.py
+++ weather/views.py
@@ -4,5 +4,25 @@ from django.conf import settings
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Live weather API</h1>")
+    # Define variables to output
+    weatherdata = {}
+    longitude = ""
+    latitude = ""
+    # Check if longitude and latitude are filled in.
+    if 'longitude' in request.GET and 'latitude' in request.GET:
+        longitude = request.GET['longitude']
+        latitude = request.GET['latitude']
+        # Create endpoint for meteoserver
+        endpoint = 'https://data.meteoserver.nl/api/liveweer_synop.php?lat={latitude}&long={longitude}&key={key}'
+        # Fill in lat, long and private key
+        url = endpoint.format(latitude=latitude, longitude=longitude, key=settings.METEO_APP_KEY)
+        # Get response
+        response = requests.get(url)
+        # Convert to json
+        weatherdata = response.json()
 
+    # Return variables to weather_base html
+    return render(request, "weather/weather_base.html", {
+        "weatherdata": weatherdata,
+        "longitude":longitude,
+        "latitude":latitude})
